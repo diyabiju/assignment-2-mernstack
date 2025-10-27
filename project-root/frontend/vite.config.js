5dc113eb1fb2bed694c5d27c5e5dd62e53bb074e
diyabiju <diyabiju45@gmail.com>
2025-10-27 12:02:42 +0530

    Update vite.config.js

--- project-root/frontend/vite.config.js
+++ project-root/frontend/vite.config.js
@@ -1,1 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3001, // React dev server port
+    proxy: {
+      "/api": "http://localhost:3000" // Proxy backend requests to port 3000
+    }
+  },
+  build: {
+    outDir: "../backend/dist"
+  }
+});
 

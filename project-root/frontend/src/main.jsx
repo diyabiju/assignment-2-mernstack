ce0de31af4c70562359f2171f9d980a577a9ad56
diyabiju <diyabiju45@gmail.com>
2025-10-27 12:02:27 +0530

    Update main.jsx

--- project-root/frontend/src/main.jsx
+++ project-root/frontend/src/main.jsx
@@ -1,1 +1,19 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./components/Home.jsx";
+import EmployeeForm from "./components/EmployeeForm.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/employee-form" element={<EmployeeForm />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
